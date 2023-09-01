b1f46296913d66ec9da12bcdcba14b670380ec02
Anders Frederik Jørgensen <afjo@tv2.dk>
2023-09-01 09:54:52 +0200

    test: Fixed AdLibPieceIdentifierComponent tests.
    The testbed was configured prior to the test setup, making it impossible to properly mock dependencies.
    Configuration of the testbed is now moved to a separate function.

--- src/app/rundown/components/ad-lib-piece-identifier/ad-lib-piece-identifier.component.spec.ts
+++ src/app/rundown/components/ad-lib-piece-identifier/ad-lib-piece-identifier.component.spec.ts
@@ -1,26 +1,31 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { AdLibPieceIdentifierComponent } from './ad-lib-piece-identifier.component';
 import { instance, mock } from '@typestrong/ts-mockito'
-import { Identifier } from '../../../core/models/identifier'
+import { AdLibPieceService } from '../../../core/services/ad-lib-piece.service'
 
 describe('AdLibPieceIdentifierComponent', () => {
-  let component: AdLibPieceIdentifierComponent;
-  let fixture: ComponentFixture<AdLibPieceIdentifierComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AdLibPieceIdentifierComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(AdLibPieceIdentifierComponent);
-    component = fixture.componentInstance;
-  });
-
-  it('should create', () => {
-    const mockedIdentifier = mock<Identifier>()
-    component.adLibPieceIdentifier = instance(mockedIdentifier)
+  it('should create', async () => {
+    const component = await configureTestBed()
     expect(component).toBeTruthy();
-  });
-});
+  })
+})
+
+async function configureTestBed(params: { maybeAdLibPieceServiceMock?: AdLibPieceService } = {}): Promise<AdLibPieceIdentifierComponent> {
+  const adLibPieceServiceMock = params.maybeAdLibPieceServiceMock ?? createMockOfAdLibPieceService()
+  await TestBed
+    .configureTestingModule({
+      providers: [
+        { provide: AdLibPieceService, useValue: instance(adLibPieceServiceMock) }
+      ],
+      declarations: [AdLibPieceIdentifierComponent]
+    })
+    .compileComponents()
+
+  const fixture: ComponentFixture<AdLibPieceIdentifierComponent> = TestBed.createComponent(AdLibPieceIdentifierComponent)
+  return fixture.componentInstance
+}
+
+function createMockOfAdLibPieceService(): AdLibPieceService {
+  return mock<AdLibPieceService>()
+}
