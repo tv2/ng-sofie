1913c77daf613aafa250af0a7e7a3bc32f8f5201
Anders Frederik Jørgensen <afjo@tv2.dk>
2023-09-01 10:36:41 +0200

    test: Fixed missing dependencies for RundownOverviewComponent tests.

--- src/app/rundown-overview/components/rundown-overview/rundown-overview.component.spec.ts
+++ src/app/rundown-overview/components/rundown-overview/rundown-overview.component.spec.ts
@@ -1,25 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RundownOverviewComponent } from './rundown-overview.component';
-import { RouterModule } from '@angular/router'
+import { RundownPlaylistService } from '../../../core/services/rundown-playlist.service'
+import { instance, mock } from '@typestrong/ts-mockito'
+import { RundownService } from '../../../core/services/rundown.service'
+import { DialogService } from '../../../shared/services/dialog.service'
 
 describe('RundownOverviewComponent', () => {
-  let component: RundownOverviewComponent;
-  let fixture: ComponentFixture<RundownOverviewComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RouterModule],
-      declarations: [ RundownOverviewComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(RundownOverviewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
+  it('should create', async () => {
+    const component = await configureTestBed()
     expect(component).toBeTruthy();
   });
 });
+
+async function configureTestBed(params: { mockedRundownPlaylistService?: RundownPlaylistService, mockedRundownService?: RundownService, mockedDialogService?: DialogService } = {}): Promise<RundownOverviewComponent> {
+  const mockedRundownPlaylistService = params.mockedRundownPlaylistService ?? createMockOfRundownPlaylistService()
+  const mockedRundownService = params.mockedRundownService ?? createMockOfRundownService()
+  const mockedDialogService = params.mockedDialogService ?? createMockOfDialogService()
+  await TestBed
+      .configureTestingModule({
+        providers: [
+          { provide: RundownPlaylistService, useValue: instance(mockedRundownPlaylistService) },
+          { provide: RundownService, useValue: instance(mockedRundownService) },
+          { provide: DialogService, useValue: instance(mockedDialogService) },
+        ],
+        declarations: [RundownOverviewComponent]
+      })
+      .compileComponents()
+
+  const fixture: ComponentFixture<RundownOverviewComponent> = TestBed.createComponent(RundownOverviewComponent)
+  return fixture.componentInstance
+}
+
+function createMockOfRundownPlaylistService(): RundownPlaylistService {
+  return mock<RundownPlaylistService>()
+}
+
+function createMockOfRundownService(): RundownService {
+    return mock<RundownService>()
+}
+
+function createMockOfDialogService(): DialogService {
+    return mock<DialogService>()
+}
