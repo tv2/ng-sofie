c9f79b62efcf3e0083f0d17ac395bc466a8866cd
Anders Frederik Jørgensen <afjo@tv2.dk>
2023-09-01 12:32:14 +0200

    test: Fixed missing dependencies and mocks for RundownStateService tests.

--- src/app/core/services/rundown-state.service.spec.ts
+++ src/app/core/services/rundown-state.service.spec.ts
@@ -1,16 +1,21 @@
-import { TestBed } from '@angular/core/testing';
-
-import { RundownStateService } from './rundown-state.service';
+import { RundownStateService } from './rundown-state.service'
+import { instance, mock } from '@typestrong/ts-mockito'
+import { RundownService } from './rundown.service'
+import { RundownEventService } from './rundown-event.service'
 
 describe('RundownStateService', () => {
-  let service: RundownStateService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(RundownStateService);
-  });
-
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    const mockedRundownService = createMockOfRundownService()
+    const mockedRundownEventService = createMockOfRundownEventService()
+    const service = new RundownStateService(instance(mockedRundownService), instance(mockedRundownEventService))
+    expect(service).toBeTruthy()
+  })
+})
+
+function createMockOfRundownService(): RundownService {
+  return mock<RundownService>()
+}
+
+function createMockOfRundownEventService(): RundownEventService {
+  return mock<RundownEventService>()
+}
