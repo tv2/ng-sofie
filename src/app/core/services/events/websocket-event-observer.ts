661f2778453ef2a21a012ab5d362c6dfbeb7a753
Anders Frederik Jørgensen <afjo@tv2.dk>
2023-09-05 09:07:16 +0200

    chore: Added debug log for WebSocketEventObserver.parseAndPublishEvent.

--- src/app/core/services/events/websocket-event-observer.ts
+++ src/app/core/services/events/websocket-event-observer.ts
@@ -23,6 +23,7 @@ export class WebSocketEventObserver implements EventObserver {
     private parseAndPublishEvent(message: MessageEvent): void {
         try {
             const event: TypedEvent = this.parseEvent(message.data)
+            console.log('[debug]', '[WebSocketEventObserver] Received an event:', message)
             this.publishEvent(event)
         } catch (error) {
             console.error('[error]', 'Failed to parse event.', error)
