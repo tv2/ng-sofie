21b8a4906c8555e1e31ae735cbdde2df27c6e21d
Anders Frederik Jørgensen <afjo@tv2.dk>
2023-09-05 09:14:10 +0200

    chore: Added remaining event handlers to RundownEventObserver.

--- src/app/core/services/events/rundown-event-observer.service.ts
+++ src/app/core/services/events/rundown-event-observer.service.ts
@@ -1,48 +1,75 @@
 import { EventConsumer, EventObserver, TypedEvent, Unsubscribe } from './event-observer.service'
 import { Injectable } from '@angular/core'
-
-enum RundownEventType {
-    ACTIVATED = 'ACTIVATE',
-    DEACTIVATED = 'DEACTIVATE',
-}
-
-interface ActivationEvent {
-    isActive: boolean
-    rundownId: string
-}
+import { RundownEventParser } from './rundown-event-parser.service'
+import { RundownEventType } from '../../models/rundown-event-type'
+import {
+    RundownActivatedEvent, RundownAdLibPieceInserted,
+    RundownDeactivatedEvent, RundownInfinitePieceAddedEvent,
+    RundownResetEvent, RundownSetNextEvent,
+    RundownTakenEvent
+} from '../../models/rundown-event'
 
 @Injectable()
 export class RundownEventObserver {
-    constructor(private readonly eventObserver: EventObserver) {}
+    constructor(private readonly eventObserver: EventObserver, private readonly rundownEventParser: RundownEventParser) {}
 
-    public subscribeToRundownActivation(consumer: (event: ActivationEvent) => void): Unsubscribe {
-        const unsubscribeRundownActivate: Unsubscribe = this.eventObserver.subscribe(RundownEventType.ACTIVATED, this.createRundownActivationConsumer(consumer))
-        const unsubscribeRundownDeactivate: Unsubscribe = this.eventObserver.subscribe(RundownEventType.DEACTIVATED, this.createRundownActivationConsumer(consumer))
-        return () => {
-            unsubscribeRundownActivate()
-            unsubscribeRundownDeactivate()
-        }
+    public subscribeToRundownActivation(consumer: (event: RundownActivatedEvent) => void): Unsubscribe {
+        return this.eventObserver.subscribe(
+            RundownEventType.ACTIVATED,
+            this.createEventValidatingConsumer(consumer, this.rundownEventParser.parseActivatedEvent)
+        )
     }
 
-    private createRundownActivationConsumer(consumer: (event: ActivationEvent) => void): EventConsumer {
+    public subscribeToRundownDeactivation(consumer: (event: RundownDeactivatedEvent) => void): Unsubscribe {
+        return this.eventObserver.subscribe(
+            RundownEventType.DEACTIVATED,
+            this.createEventValidatingConsumer(consumer, this.rundownEventParser.parseDeactivatedEvent)
+        )
+    }
+
+    public subscribeToRundownReset(consumer: (event: RundownResetEvent) => void): Unsubscribe {
+        return this.eventObserver.subscribe(
+            RundownEventType.RESET,
+            this.createEventValidatingConsumer(consumer, this.rundownEventParser.parseResetEvent)
+        )
+    }
+
+    public subscribeToRundownTake(consumer: (event: RundownTakenEvent) => void): Unsubscribe {
+        return this.eventObserver.subscribe(
+            RundownEventType.TAKEN,
+            this.createEventValidatingConsumer(consumer, this.rundownEventParser.parseTakenEvent)
+        )
+    }
+
+    public subscribeToRundownSetNext(consumer: (event: RundownSetNextEvent) => void): Unsubscribe {
+        return this.eventObserver.subscribe(
+            RundownEventType.SET_NEXT,
+            this.createEventValidatingConsumer(consumer, this.rundownEventParser.parseSetNextEvent)
+        )
+    }
+
+    public subscribeToRundownAdLibPieceInserted(consumer: (event: RundownAdLibPieceInserted) => void): Unsubscribe {
+        return this.eventObserver.subscribe(
+            RundownEventType.AD_LIB_PIECE_INSERTED,
+            this.createEventValidatingConsumer(consumer, this.rundownEventParser.parseAdLibPieceInserted)
+        )
+    }
+
+    public subscribeToRundownInfinitePieceAdded(consumer: (event: RundownInfinitePieceAddedEvent) => void): Unsubscribe {
+        return this.eventObserver.subscribe(
+            RundownEventType.INFINITE_PIECE_ADDED,
+            this.createEventValidatingConsumer(consumer, this.rundownEventParser.parseInfinitePieceAdded)
+        )
+    }
+
+    private createEventValidatingConsumer<T>(consumer: (event: T) => void, parser: (maybeEvent: unknown) => T): EventConsumer {
         return (event: TypedEvent) => {
             try {
-                const activationEvent: ActivationEvent = this.parseActivationEvent(event)
+                const activationEvent: T = parser(event)
                 consumer(activationEvent)
             } catch (error) {
                 console.error('Failed to parse activation event', error, event)
             }
-        }
-    }
-
-    private parseActivationEvent(event: TypedEvent): ActivationEvent {
-        if (!('rundownId' in event)) {
-            throw new Error('rundownId is missing from rundown activation event.')
-        }
-        return {
-            isActive: event.type === RundownEventType.ACTIVATED,
-            // TODO: Add better schema validation (e.g. with zod).
-            rundownId: (event as any).rundownId
         }
     }
 }
