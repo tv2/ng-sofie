ca8478d232f2e702ba85a3a50f247e860b71b2dc
Anders Frederik Jørgensen <afjo@tv2.dk>
2023-09-11 15:14:27 +0200

    test: Fixed AppComponent test.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ConnectionErrorService } from './shared/services/connection-error.service'
+import { instance, mock } from '@typestrong/ts-mockito'
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,14 +11,31 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
-    }).compileComponents();
+    }).compileComponents()
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+  it('should create the app', async () => {
+    await configureTestBed()
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
     expect(app).toBeTruthy();
-  });
-});
+  })
+})
+async function configureTestBed(): Promise<void> {
+  const mockedConnectionErrorService: ConnectionErrorService = mock<ConnectionErrorService>()
+  await TestBed
+      .configureTestingModule({
+        imports: [
+          RouterTestingModule
+        ],
+        declarations: [
+          AppComponent,
+        ],
+        providers: [
+          { provide: ConnectionErrorService, useValue: instance(mockedConnectionErrorService) },
+        ],
+      })
+      .compileComponents()
+}
