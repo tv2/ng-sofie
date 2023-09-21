d62e4b6c9b2e2d3ca8c640f2ce81a4e908e6ca91
Anders Frederik Jørgensen <afjo@tv2.dk>
2023-09-21 12:49:18 +0200

    refactor: TimestampPipe has tests and has formatting split up for readability.

--- src/app/shared/pipes/timestamp.pipe.ts
+++ src/app/shared/pipes/timestamp.pipe.ts
@@ -6,13 +6,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TimestampPipe implements PipeTransform {
   public transform(timestampInSeconds: number): string {
     const seconds: number = timestampInSeconds % 60
-    let minutes: number = Math.floor(timestampInSeconds / 60)
-    let hours: number = 0
-    if (minutes >= 60) {
-      hours = Math.floor(minutes / 60)
-      minutes = minutes % 60
-    }
+    const minutes: number = Math.floor((timestampInSeconds % 3600) / 60)
+    const hours: number = Math.floor( timestampInSeconds / 3600)
 
-    return `${hours > 0 ? `${hours}:` : ''}${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
+    return `${this.formatHours(hours)}${this.formatMinutes(minutes)}${this.formatSeconds(seconds)}`
+  }
+
+  private formatHours(hours: number): string {
+    if (hours <= 0) {
+      return ''
+    }
+    return hours.toString().padStart(2, '0') + ':'
+  }
+
+  private formatMinutes(minutes: number): string {
+    return minutes.toString().padStart(2, '0') + ':'
+  }
+
+  private formatSeconds(seconds: number): string {
+    return seconds.toString().padStart(2, '0')
   }
 }
