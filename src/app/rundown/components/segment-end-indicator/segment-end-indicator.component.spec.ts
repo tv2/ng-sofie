3113a4163fb919041b29bffe61e81b28bfbe47bc
Anders Frederik Jørgensen <afjo@tv2.dk>
2023-09-25 13:51:57 +0200

    feat: Segment now has an end of segment indicator.

---
+++ src/app/rundown/components/segment-end-indicator/segment-end-indicator.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SegmentEndIndicatorComponent } from './segment-end-indicator.component';
+
+describe('SegmentEndIndicatorComponent', () => {
+  let component: SegmentEndIndicatorComponent;
+  let fixture: ComponentFixture<SegmentEndIndicatorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SegmentEndIndicatorComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SegmentEndIndicatorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/rundown/components/segment-end-indicator/segment-end-indicator.component.ts
@@ -0,0 +1,18 @@
+import { Component, HostBinding, Input } from '@angular/core'
+import { Part } from '../../../core/models/part'
+
+@Component({
+  selector: 'sofie-segment-end-indicator',
+  templateUrl: './segment-end-indicator.component.html',
+  styleUrls: ['./segment-end-indicator.component.scss'],
+  host: {
+    class: 'c-segment-end-indicator',
+  },
+})
+export class SegmentEndIndicatorComponent {
+  @Input()
+  public lastPartInSegment?: Part
+
+  @HostBinding('class.c-segment-end-indicator--active')
+  public get isLastPart() { return this.lastPartInSegment?.isOnAir ?? false }
+}
