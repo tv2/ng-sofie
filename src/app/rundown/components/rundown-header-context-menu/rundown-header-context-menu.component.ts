58adf6aa215c5d0a996f6273173f1318a6cce366
Rasmus <raal@tv2.dk>
2023-10-03 14:50:58 +0200

    fix: changes look of the context menu to not have the Options header

--- src/app/rundown/components/rundown-header-context-menu/rundown-header-context-menu.component.ts
+++ src/app/rundown/components/rundown-header-context-menu/rundown-header-context-menu.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, HostBinding, Input, OnInit, ViewChild} from '@angular/core';
 import {RundownService} from "../../../core/abstractions/rundown.service";
 import {Rundown} from "../../../core/models/rundown";
 
