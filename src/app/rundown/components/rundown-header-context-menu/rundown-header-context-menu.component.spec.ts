a69a4fecba7b866d067281fa96625bbe61c6fa68
Rasmus <raal@tv2.dk>
2023-10-03 11:57:09 +0200

    fix: configures test files for new components

--- src/app/rundown/components/rundown-header-context-menu/rundown-header-context-menu.component.spec.ts
+++ src/app/rundown/components/rundown-header-context-menu/rundown-header-context-menu.component.spec.ts
@@ -1,23 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RundownHeaderContextMenuComponent } from './rundown-header-context-menu.component';
+import {Rundown} from "../../../core/models/rundown";
+import {instance, mock, when} from "@typestrong/ts-mockito";
+import {RouterModule} from "@angular/router";
+import {RundownService} from "../../../core/abstractions/rundown.service";
 
 describe('HeaderContextMenuComponent', () => {
-  let component: RundownHeaderContextMenuComponent;
-  let fixture: ComponentFixture<RundownHeaderContextMenuComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ RundownHeaderContextMenuComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(RundownHeaderContextMenuComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
+  it('should create', async () => {
+    const mockedRundown: Rundown = getMockedRundown()
+    const component: RundownHeaderContextMenuComponent = await configureTestBed()
+    component.rundown = instance(mockedRundown)
+    component.ngOnInit()
     expect(component).toBeTruthy();
   });
 });
+
+async function configureTestBed(params: { mockedRundownService?: RundownService} = {}): Promise<RundownHeaderContextMenuComponent> {
+  const mockedRundownService = params.mockedRundownService ?? mock<RundownService>()
+  await TestBed
+      .configureTestingModule( {
+        imports: [RouterModule.forRoot([])],
+        providers: [
+          { provide: RundownService, useValue: instance(mockedRundownService) },
+        ],
+        declarations: [RundownHeaderContextMenuComponent]
+      })
+      .compileComponents()
+  const fixture: ComponentFixture<RundownHeaderContextMenuComponent> = TestBed.createComponent(RundownHeaderContextMenuComponent)
+  return fixture.componentInstance
+}
+
+
+function getMockedRundown(): Rundown {
+  const mockedRundown = mock<Rundown>()
+  when(mockedRundown.id).thenReturn('some-part-id')
+  return mockedRundown
+}
