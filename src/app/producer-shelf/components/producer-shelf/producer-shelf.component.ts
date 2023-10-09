87c4b51cd298c37920241441e13136f4e386462b
Rasmus <raal@tv2.dk>
2023-10-09 09:51:55 +0200

    feat: adds draghandle that can move the shelf vertically, also adds new drag icon

--- src/app/producer-shelf/components/producer-shelf/producer-shelf.component.ts
+++ src/app/producer-shelf/components/producer-shelf/producer-shelf.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core'
+import { Component, HostListener } from '@angular/core'
 import { KeyboardBindingService } from '../../abstractions/keyboard-binding.service'
 import { KeyBinding } from '../../models/key-binding'
 import { KeyboardBindingMatcher } from '../../services/keyboard-binding.matcher'
+import {IconButton, IconButtonSize} from "../../../shared/enums/icon-button";
 
 @Component({
   selector: 'sofie-producer-shelf',
@@ -11,6 +12,25 @@ import { KeyboardBindingMatcher } from '../../services/keyboard-binding.matcher'
 export class ProducerShelfComponent {
   public keyBindings: KeyBinding[] = []
   public pressedKeys: string[] = []
+
+  @HostListener('mousedown', ['$event'])
+  public onDragStart(event: MouseEvent): void {
+    this.verticalDragStartPoint = event.clientY
+    const onDragMove: (event: MouseEvent) => void = this.onDragMove.bind(this)
+    window.addEventListener('mousemove', onDragMove)
+    window.addEventListener(
+      'mouseup',
+      () => {
+        this.verticalDragStartPoint = undefined
+        window.removeEventListener('mousemove', onDragMove)
+      },
+      { once: true }
+    )
+  }
+
+  public scrollOffset: number = 0
+
+  private verticalDragStartPoint?: number
 
   constructor(
     private readonly keyboardBindingService: KeyboardBindingService,
@@ -28,4 +48,19 @@ export class ProducerShelfComponent {
   public isKeyBindingMatched(keyBinding: KeyBinding): boolean {
     return this.keyboardBindingMatcher.isKeyBindingMatchedExactly(keyBinding, this.pressedKeys)
   }
+
+  public onDragMove(event: MouseEvent): void {
+    //TODO: change to use the offset height from bottom to cursor, so we can drag off screen.
+    event.preventDefault()
+    if (!this.verticalDragStartPoint) {
+      return
+    }
+    const newVerticalPoint: number = event.clientY
+    const verticalDeltaInPixels: number = this.verticalDragStartPoint - newVerticalPoint
+    this.verticalDragStartPoint = newVerticalPoint
+    this.scrollOffset = this.scrollOffset + verticalDeltaInPixels
+  }
+
+  protected readonly IconButton = IconButton;
+  protected readonly IconButtonSize = IconButtonSize;
 }
--- src/app/producer-shelf/producer-shelf.module.ts
+++ src/app/producer-shelf/producer-shelf.module.ts
@@ -5,10 +5,11 @@ import { KeyboardBindingService } from './abstractions/keyboard-binding.service'
 import { HardcodedProducerKeyboardBindingService } from './services/hardcoded-producer-keyboard-binding.service'
 import { KeyboardBindingMatcher } from './services/keyboard-binding.matcher'
 import { CameraKeyBindingFactory } from './factories/camera-key-binding.factory'
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
   declarations: [ProducerShelfComponent],
-  imports: [CommonModule],
+    imports: [CommonModule, SharedModule],
   providers: [
     { provide: KeyboardBindingService, useClass: HardcodedProducerKeyboardBindingService },
     KeyboardBindingMatcher,
