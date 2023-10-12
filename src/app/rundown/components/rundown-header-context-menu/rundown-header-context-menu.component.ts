97bd40bf0606e7ee694e7e5e6db5830fa200d0a6
Rasmus <raal@tv2.dk>
2023-10-12 14:11:34 +0200

    refactor: changes the header context men to wrap the generic component

--- src/app/rundown/components/rundown-header-context-menu/rundown-header-context-menu.component.ts
+++ src/app/rundown/components/rundown-header-context-menu/rundown-header-context-menu.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
 import { RundownService } from '../../../core/abstractions/rundown.service'
 import { Rundown } from '../../../core/models/rundown'
 import { DialogService } from '../../../shared/services/dialog.service'
+import { ContextMenuOption } from '../../../shared/abstractions/context-menu-option'
 
 @Component({
   selector: 'sofie-rundown-header-context-menu',
@@ -13,6 +14,32 @@ export class RundownHeaderContextMenuComponent {
   @Input()
   public rundown?: Rundown
 
+  public readonly deactivatedContextMenuOptions: ContextMenuOption[] = [
+    {
+      label: 'Activate (On Air)',
+      contextAction: (): void => this.openActivationDialog(),
+    },
+    {
+      label: 'Reset Rundown',
+      contextAction: (): void => this.resetRundown(),
+    },
+  ]
+
+  public readonly activatedContextMenuOptions: ContextMenuOption[] = [
+    {
+      label: 'Deactivate',
+      contextAction: (): void => this.openDeactivationDialog(),
+    },
+    {
+      label: 'Take',
+      contextAction: (): void => this.take(),
+    },
+    {
+      label: 'Reset Rundown',
+      contextAction: (): void => this.resetRundown(),
+    },
+  ]
+
   constructor(
     private readonly rundownService: RundownService,
     private readonly dialogService: DialogService
@@ -23,6 +50,13 @@ export class RundownHeaderContextMenuComponent {
       return
     }
     this.dialogService.createConfirmDialog(this.rundown.name, `Are you sure you want to activate the Rundown?`, 'Activate', () => this.activate())
+  }
+
+  public openDeactivationDialog(): void {
+    if (!this.rundown || !this.rundown.isActive) {
+      return
+    }
+    this.dialogService.createConfirmDialog(this.rundown.name, `Are you sure you want to deactivate this Rundown?`, 'Deactivate', () => this.deactivate())
   }
 
   public activate(): void {
