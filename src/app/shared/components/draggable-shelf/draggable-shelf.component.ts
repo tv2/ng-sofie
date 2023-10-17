125fd8c81c7a12377a95fc4138932d86e15a74b5
Anders Frederik Jørgensen <afjo@tv2.dk>
2023-10-17 13:36:38 +0200

    feat: Made draggable shelf collapsable while remembering size. Double clicking the drag handle toggles whether it is collapsed or not.

--- src/app/shared/components/draggable-shelf/draggable-shelf.component.ts
+++ src/app/shared/components/draggable-shelf/draggable-shelf.component.ts
@@ -1,14 +1,18 @@
 import { Component, ElementRef, HostBinding, HostListener, Input, OnInit, ViewChild } from '@angular/core'
 import { IconButton, IconButtonSize } from '../../enums/icon-button'
 
+interface PositionData {
+  heightInPixels: number,
+  isCollapsed: boolean,
+}
+
 @Component({
   selector: 'sofie-draggable-shelf',
   templateUrl: './draggable-shelf.component.html',
   styleUrls: ['./draggable-shelf.component.scss'],
 })
 export class DraggableShelfComponent implements OnInit {
-  @HostBinding('style.flex-basis.px')
-  @HostBinding('style.height.px')
+  public isCollapsed: boolean = false
   public heightInPixels: number = 0
   public verticalDragPositionFromDragHandleBottomInPixels?: number
 
@@ -19,28 +23,36 @@ export class DraggableShelfComponent implements OnInit {
   private readonly dragHandleElement: ElementRef<HTMLDivElement>
 
   @Input()
-  public localStorageKeyForHeight?: string
+  public localStorageKey?: string
 
   public ngOnInit(): void {
-    this.heightInPixels = this.getInitialHeight()
+    this.loadInitialPosition()
   }
 
-  private getInitialHeight(): number {
-    if (!this.localStorageKeyForHeight) {
-      return 0
+  private loadInitialPosition(): void {
+    if (!this.localStorageKey) {
+      return
     }
 
-    const storedHeight: string | null = window.localStorage.getItem(this.localStorageKeyForHeight)
-    if (!storedHeight) {
-      return 0
+    const positionDataAsText: string | null = window.localStorage.getItem(this.localStorageKey)
+    if (!positionDataAsText) {
+      return
     }
 
-    const storedHeightInPixels: number = Number(storedHeight)
-    if (Number.isNaN(storedHeight)) {
+    try {
+      const positionData: PositionData = JSON.parse(positionDataAsText)
+      this.isCollapsed = positionData.isCollapsed
+      this.heightInPixels = positionData.heightInPixels
+    } catch {}
+  }
+
+  @HostBinding('style.flex-basis.px')
+  @HostBinding('style.height.px')
+  public get displayHeight(): number {
+    if (this.isCollapsed) {
       return 0
     }
-
-    return storedHeightInPixels
+    return this.heightInPixels
   }
 
   @HostListener('mousedown', ['$event'])
@@ -57,28 +69,35 @@ export class DraggableShelfComponent implements OnInit {
       () => {
         this.verticalDragPositionFromDragHandleBottomInPixels = undefined
         window.removeEventListener('mousemove', onDragMove)
-        this.storeHeight()
+        this.storePositionData()
       },
       { once: true }
     )
   }
 
-  private storeHeight(): void {
-    if (!this.localStorageKeyForHeight) {
+  private storePositionData(): void {
+    if (!this.localStorageKey) {
       return
     }
-    window.localStorage.setItem(this.localStorageKeyForHeight, this.heightInPixels.toString())
+    const positionData: PositionData = {
+      heightInPixels: this.heightInPixels,
+      isCollapsed: this.isCollapsed,
+    }
+    window.localStorage.setItem(this.localStorageKey, JSON.stringify(positionData))
   }
 
   public onDragMove(event: MouseEvent): void {
+    if (this.isCollapsed) {
+      this.toggleCollapse()
+    }
     if (this.verticalDragPositionFromDragHandleBottomInPixels === undefined) {
       return
     }
     this.heightInPixels = window.innerHeight - event.clientY + this.verticalDragPositionFromDragHandleBottomInPixels
   }
 
-  protected collapse(): void {
-    this.heightInPixels = 0
-    this.storeHeight()
+  protected toggleCollapse(): void {
+    this.isCollapsed = !this.isCollapsed
+    this.storePositionData()
   }
 }
