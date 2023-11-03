bef07d6b1dd7a86a922d0776af8e0c0415ae56f8
Anders Frederik Jørgensen <afjo@tv2.dk>
2023-11-03 07:50:12 +0100

    refactor: Tv2ActionGroupService uses switch case with type assertions instead of type guards.

--- src/app/rundown-view/services/tv2-action-group.service.ts
+++ src/app/rundown-view/services/tv2-action-group.service.ts
@@ -13,32 +13,20 @@ export class Tv2ActionGroupService {
   }
 
   private groupActionsByContentTypeReducer(grouping: Tv2ActionContentTypeGrouping, action: Tv2Action): Tv2ActionContentTypeGrouping {
-    if (this.isCameraAction(action)) {
-      grouping.camera.push(action)
-    } else if (this.isRemoteAction(action)) {
-      grouping.remote.push(action)
-    } else if (this.isVideoClipAction(action)) {
-      grouping.videoClip.push(action)
-    } else if (this.isTransitionAction(action)) {
-      grouping.transition.push(action)
+    switch (action.metadata.contentType) {
+      case Tv2ActionContentType.CAMERA:
+        grouping.camera.push(action as Tv2CameraAction)
+        break
+      case Tv2ActionContentType.REMOTE:
+        grouping.remote.push(action as Tv2RemoteAction)
+        break
+      case Tv2ActionContentType.VIDEO_CLIP:
+        grouping.videoClip.push(action as Tv2VideoClipAction)
+        break
+      case Tv2ActionContentType.TRANSITION:
+        grouping.transition.push(action as Tv2TransitionAction)
     }
     return grouping
-  }
-
-  private isCameraAction(action: Tv2Action): action is Tv2CameraAction {
-    return action.metadata.contentType === Tv2ActionContentType.CAMERA
-  }
-
-  private isRemoteAction(action: Tv2Action): action is Tv2RemoteAction {
-    return action.metadata.contentType === Tv2ActionContentType.REMOTE
-  }
-
-  private isVideoClipAction(action: Tv2Action): action is Tv2VideoClipAction {
-    return action.metadata.contentType === Tv2ActionContentType.VIDEO_CLIP
-  }
-
-  private isTransitionAction(action: Tv2Action): action is Tv2TransitionAction {
-    return action.metadata.contentType === Tv2ActionContentType.TRANSITION
   }
 
   private createEmptyGrouping(): Tv2ActionContentTypeGrouping {
