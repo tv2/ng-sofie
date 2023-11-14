1c92f177cb0e62276f7c0fee1621de8aa452658a
Rasmus <raal@tv2.dk>
2023-11-14 08:52:07 +0100

    WIP: started on new tests

--- src/app/core/services/rundown-timing.service.spec.ts
+++ src/app/core/services/rundown-timing.service.spec.ts
@@ -21,11 +21,13 @@ describe(RundownTimingService.name, () => {
           const durationInMsToPlannedEndFromNow: number = 10_000
           const rundownExpectedDurationInMs: number = 30_000
           const numberOfSegments: number = 4
-          const segments: Segment[] = [...Array(numberOfSegments)].map((_, index) => testEntityFactory.createSegment({ id: index.toString(), budgetDuration: rundownExpectedDurationInMs / numberOfSegments }))
+          const segments: Segment[] = [...Array(numberOfSegments)].map((_, index) =>
+            testEntityFactory.createSegment({ id: index.toString(), budgetDuration: rundownExpectedDurationInMs / numberOfSegments })
+          )
           const timing: RundownTiming = {
             type: RundownTimingType.BACKWARD,
             expectedEndEpochTime: Date.now() + durationInMsToPlannedEndFromNow,
-            expectedDurationInMs: rundownExpectedDurationInMs
+            expectedDurationInMs: rundownExpectedDurationInMs,
           }
           const rundown: Rundown = testEntityFactory.createRundown({ isActive: false, segments, timing })
           const testee: RundownTimingService = createTestee()
@@ -44,11 +46,13 @@ describe(RundownTimingService.name, () => {
           const rundownExpectedDurationInMs: number = 30_000
           const numberOfSegments: number = 4
           const testEntityFactory: TestEntityFactory = new TestEntityFactory()
-          const segments: Segment[] = [...Array(numberOfSegments)].map((_, index) => testEntityFactory.createSegment({ id: index.toString(), budgetDuration: rundownExpectedDurationInMs / numberOfSegments }))
+          const segments: Segment[] = [...Array(numberOfSegments)].map((_, index) =>
+            testEntityFactory.createSegment({ id: index.toString(), budgetDuration: rundownExpectedDurationInMs / numberOfSegments })
+          )
           const timing: RundownTiming = {
             type: RundownTimingType.BACKWARD,
             expectedEndEpochTime: Date.now() + durationInMsToPlannedEndFromNow,
-            expectedDurationInMs: rundownExpectedDurationInMs
+            expectedDurationInMs: rundownExpectedDurationInMs,
           }
           const rundown: Rundown = testEntityFactory.createRundown({ isActive: false, segments, timing })
           const testee: RundownTimingService = createTestee()
@@ -65,43 +69,97 @@ describe(RundownTimingService.name, () => {
     describe('when rundown is active', () => {
       describe('when rundown has no segment on air', () => {
         describe('when rundown has no continuity segment', () => {
-          it('returns the difference with remaining time being based on all subsequent segments from the start of the next segment', () => {})
-          })
-        })
+          it('returns the difference with remaining time being based on all subsequent segments from the start of the next segment', () => {
+            const durationInMsToPlannedEndFromNow: number = 10_000
+            const rundownExpectedDurationInMs: number = 30_000
+            const numberOfSegments: number = 4
+            const testEntityFactory: TestEntityFactory = new TestEntityFactory()
+            const segments: Segment[] = [...Array(numberOfSegments)].map((_, index) =>
+              testEntityFactory.createSegment({ id: index.toString(), budgetDuration: rundownExpectedDurationInMs / numberOfSegments })
+            )
+            const nextSegmentDuration: number = 2_000
+            const nextSegment: Segment = testEntityFactory.createSegment({ id: 'nextSegment', isNext: true, budgetDuration: nextSegmentDuration })
+            segments.unshift(nextSegment)
+            const timing: RundownTiming = {
+              type: RundownTimingType.BACKWARD,
+              expectedEndEpochTime: Date.now() + durationInMsToPlannedEndFromNow,
+              expectedDurationInMs: rundownExpectedDurationInMs,
+            }
+            const rundown: Rundown = testEntityFactory.createRundown({ isActive: true, segments, timing })
+            const testee: RundownTimingService = createTestee()
+            const estimateEndEpocTime: number = Date.now() + rundownExpectedDurationInMs + nextSegmentDuration
+            const expectedResult: number = estimateEndEpocTime - timing.expectedEndEpochTime
 
-        describe('when rundown has one continuity segment', () => {
-          it('returns the difference with remaining time being based on all subsequent segments between the start of the next segment and the continuity segment', () => {})
+            const result: number = testee.getRundownScheduleOffsetInMs(rundown)
+
+            expect(result).toBe(expectedResult)
+          })
         })
       })
 
-      describe('when rundown has a segment on air', () => {
-        describe('when rundown has no continuity segment', () => {
-          describe('when next segment is before the on air segment', () => {
-            it('?????', () => {})
-          })
+      describe('when rundown has one continuity segment', () => {
+        it('returns the difference with remaining time being based on all subsequent segments between the start of the next segment and the continuity segment', () => {
+          const durationInMsToPlannedEndFromNow: number = 12_000
+          const rundownExpectedDurationInMs: number = 30_000
+          const numberOfSegments: number = 5
+          const testEntityFactory: TestEntityFactory = new TestEntityFactory()
+          const segments: Segment[] = [...Array(numberOfSegments)].map((_, index) =>
+            testEntityFactory.createSegment({ id: index.toString(), budgetDuration: rundownExpectedDurationInMs / numberOfSegments })
+          )
 
-          describe('when next segment is the on air segment', () => {
-            it('?????', () => {})
-          })
+          const nextSegmentDuration: number = 2_000
+          const nextSegment: Segment = testEntityFactory.createSegment({ id: 'nextSegment', isNext: true, budgetDuration: nextSegmentDuration })
+          segments.unshift(nextSegment)
+          const continuitySegment: Segment = testEntityFactory.createSegment({ name: 'CONTINUITY' })
+          segments.push(continuitySegment)
+          // TODO: Insert segment after continuity
 
-          describe('when next segment is after the on air segment', () => {
-            it('?????', () => {})
-          })
+          const timing: RundownTiming = {
+            type: RundownTimingType.BACKWARD,
+            expectedEndEpochTime: Date.now() + durationInMsToPlannedEndFromNow,
+            expectedDurationInMs: rundownExpectedDurationInMs,
+          }
+          const rundown: Rundown = testEntityFactory.createRundown({ isActive: true, segments, timing })
+          const testee: RundownTimingService = createTestee()
+
+          const estimateEndEpocTime: number = Date.now() + rundownExpectedDurationInMs + nextSegmentDuration
+          const expectedResult: number = estimateEndEpocTime - timing.expectedEndEpochTime
+
+          const result: number = testee.getRundownScheduleOffsetInMs(rundown)
+
+          expect(result).toBe(expectedResult)
+        })
+      })
+    })
+
+    describe('when rundown has a segment on air', () => {
+      describe('when rundown has no continuity segment', () => {
+        describe('when next segment is before the on air segment', () => {
+          it('?????', () => {})
         })
 
-        describe('when rundown has one continuity segment', () => {
-          describe('when next segment is before the on air segment', () => {
-            it('?????', () => {})
-          })
-
-          describe('when next segment is the on air segment', () => {
-            it('?????', () => {})
-          })
-
-          describe('when next segment is after the on air segment', () => {
-            it('?????', () => {})
-          })
+        describe('when next segment is the on air segment', () => {
+          it('?????', () => {})
         })
+
+        describe('when next segment is after the on air segment', () => {
+          it('?????', () => {})
+        })
+      })
+
+      describe('when rundown has one continuity segment', () => {
+        describe('when next segment is before the on air segment', () => {
+          it('?????', () => {})
+        })
+
+        describe('when next segment is the on air segment', () => {
+          it('?????', () => {})
+        })
+
+        describe('when next segment is after the on air segment', () => {
+          it('?????', () => {})
+        })
+      })
     })
   })
 })
