969cea9b68d7ed01204b51e4c2a55302f2019a11
Anders Frederik Jørgensen <afjo@tv2.dk>
2023-11-20 08:14:47 +0100

    chore: Removed dead code for TimerPipe and RundownTimingService.

--- src/app/core/services/rundown-timing.service.ts
+++ src/app/core/services/rundown-timing.service.ts
@@ -9,31 +9,8 @@ import { Injectable } from '@angular/core'
 export class RundownTimingService {
   constructor(private readonly partEntityService: PartEntityService) {}
 
-  public getRundownScheduleOffsetInMs(rundown: Rundown): number {
-    if (!rundown.isActive) {
-      const rundownPlannedEndEpochTime: number = this.getAggregatedEndEpochTime(rundown)
-      const estimatedRundownEndEpochTime: number = Date.now() + this.getAggregatedDurationInMs(rundown)
-      return estimatedRundownEndEpochTime - rundownPlannedEndEpochTime
-    }
-
-    const remainingDuration: number = this.getRemainingRundownDuration(rundown)
-    const estimatedEndEpochTime: number = Date.now() + remainingDuration
-    return estimatedEndEpochTime - this.getAggregatedEndEpochTime(rundown)
-  }
-
-  private getAggregatedEndEpochTime(rundown: Rundown): number {
-    switch (rundown.timing.type) {
-      case RundownTimingType.FORWARD:
-        return rundown.timing.expectedEndEpochTime ?? rundown.timing.expectedStartEpochTime + this.getAggregatedDurationInMs(rundown)
-      case RundownTimingType.BACKWARD:
-        return rundown.timing.expectedEndEpochTime
-      case RundownTimingType.UNSCHEDULED:
-        return Date.now() + this.getRemainingRundownDuration(rundown)
-    }
-  }
-
-  private getAggregatedDurationInMs(rundown: Rundown): number {
-    return rundown.timing.expectedDurationInMs ?? rundown.segments.reduce((accumulatedDurationInMs, segment) => accumulatedDurationInMs + this.getExpectedDurationInMsForSegment(segment), 0)
+  public getExpectedDurationInMsForSegments(rundown: Rundown): Record<string, number> {
+    return Object.fromEntries(rundown.segments.map(segment => [segment.id, this.getExpectedDurationInMsForSegment(segment)]))
   }
 
   public getExpectedDurationInMsForSegment(segment: Segment): number {
@@ -41,34 +18,6 @@ export class RundownTimingService {
       return 0
     }
     return segment.budgetDuration ?? segment.parts.reduce((accumulatedPartDuration: number, part: Part) => accumulatedPartDuration + (part.expectedDuration ?? 0), 0)
-  }
-
-  private getRemainingRundownDuration(rundown: Rundown): number {
-    const onAirSegment: Segment | undefined = rundown.segments.filter(segment => !segment.isUntimed).find(segment => segment.isOnAir)
-    const remainingOnAirSegmentBudgetDuration: number = onAirSegment?.budgetDuration ? Math.max(0, onAirSegment.budgetDuration - this.getOnAirSegmentPlayedDuration(onAirSegment)) : 0
-
-    const nextSegmentIndex: number = rundown.segments.findIndex(segment => segment.isNext)
-    if (nextSegmentIndex < 0) {
-      return remainingOnAirSegmentBudgetDuration
-    }
-    const segmentBudgetRemainingFromNext: number = rundown.segments
-      .slice(nextSegmentIndex)
-      .filter(segment => !segment.isOnAir && !segment.isUntimed)
-      .reduce((accumulatedEndEpochTime: number, segment: Segment) => accumulatedEndEpochTime + this.getExpectedDurationInMsForSegment(segment), 0)
-    return segmentBudgetRemainingFromNext + remainingOnAirSegmentBudgetDuration
-  }
-
-  private getOnAirSegmentPlayedDuration(onAirSegment: Segment): number {
-    const onAirPartIndex: number = onAirSegment.parts.findIndex(part => part.isOnAir)
-    const playedPartDuration: number = onAirSegment.parts
-      .slice(0, onAirPartIndex)
-      .reduce((accumulatedPartDuration: number, part: Part) => accumulatedPartDuration + this.partEntityService.getDuration(part), 0)
-    const onAirPartPlayedDuration: number = this.partEntityService.getPlayedDuration(onAirSegment.parts[onAirPartIndex])
-    return playedPartDuration + onAirPartPlayedDuration
-  }
-
-  public getExpectedDurationInMsForSegments(rundown: Rundown): Record<string, number> {
-    return Object.fromEntries(rundown.segments.map(segment => [segment.id, this.getExpectedDurationInMsForSegment(segment)]))
   }
 
   public getExpectedDurationInMsForRundown(rundown: Rundown, expectedDurationsInMsForSegments: Record<string, number>): number {
--- src/app/shared/pipes/timer/timer.pipe.ts
+++ src/app/shared/pipes/timer/timer.pipe.ts
@@ -111,23 +111,4 @@ export class TimerPipe implements PipeTransform {
 
     return format
   }
-
-  //public transform(durationInMs: number, format: string = 'HH:mm:ss'): string {
-  //  const durationInSeconds: number = Math.floor(durationInMs / 1000)
-  //  const absoluteDurationInSeconds: number = Math.abs(durationInSeconds)
-  //  const seconds: number = absoluteDurationInSeconds % 60
-  //  const minutes: number = Math.floor((absoluteDurationInSeconds % 3600) / 60)
-  //  const hours: number = Math.floor(absoluteDurationInSeconds / 3600)
-  //  const sign: string = this.getSign(durationInSeconds, format)
-  //  return `${sign}${this.formatDigits(hours)}:${this.formatDigits(minutes)}:${this.formatDigits(seconds)}`
-  //}
-
-  //private getSign(durationInSeconds: number, signsToUse: string): string {
-  //  const sign: string = durationInSeconds <= 0 ? NEGATIVE_SIGN : POSITIVE_SIGN
-  //  return signsToUse.includes(sign) ? sign : ''
-  //}
-
-  //private formatDigits(digits: number): string {
-  //  return digits.toString().padStart(2, '0')
-  //}
 }
