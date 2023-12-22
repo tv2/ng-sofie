1d946746ff36cc09b1da2bdc6a8aafe76efba5b0
Dimitar G. Moustakinov <k-primeholding-dim@tv2.dk>
2023-12-22 09:55:14 +0200

    SOF-1754 - service test adjustments

--- src/app/settings/services/http-configuration-cache.service.spec.ts
+++ src/app/settings/services/http-configuration-cache.service.spec.ts
@@ -4,18 +4,20 @@ import { HttpConfigurationCacheService } from './http-configuration-cache.servic
 import { HttpClient } from '@angular/common/http'
 import { HttpErrorService } from '../../shared/services/http/http-error.service'
 
-describe('HttpConfigurationCacheService', () => {
-  it('should be created', () => {
-    const mockLogger: Logger = mock<Logger>()
-    const mockHttpClient: HttpClient = mock<HttpClient>()
-    const mockHttpErrorService: HttpErrorService = mock<HttpErrorService>()
-    const testee: HttpConfigurationCacheService = new HttpConfigurationCacheService(
-      instance(mockLogger),
-      instance(mockHttpClient),
-      instance(mockHttpErrorService),
-    )
-    expect(testee).toBeTruthy()
-    testee.postClearConfigurationCache()
-    verify(mockHttpClient.post).once()
+describe(HttpConfigurationCacheService.name, () => {
+  describe(HttpConfigurationCacheService.prototype.postClearConfigurationCache.name, () => {
+    it('should perform post upon invocation', () => {
+      const mockLogger: Logger = mock<Logger>()
+      const mockHttpClient: HttpClient = mock<HttpClient>()
+      const mockHttpErrorService: HttpErrorService = mock<HttpErrorService>()
+      const testee: HttpConfigurationCacheService = new HttpConfigurationCacheService(
+        instance(mockLogger),
+        instance(mockHttpClient),
+        instance(mockHttpErrorService)
+      )
+      expect(testee).toBeTruthy()
+      testee.postClearConfigurationCache()
+      verify(mockHttpClient.post).once()
+    })
   })
 })
