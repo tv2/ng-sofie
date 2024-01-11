425e0db9d00c2b3a117a5db0595eab94b274b0f3
Dimitar G. Mustakinov <k-primeholding-dim@tv2.dk>
2024-01-11 17:47:18 +0200

    SOF-1720 - lint-fixes

--- src/app/rundown-view/components/minishelf/minishelf.component.ts
+++ src/app/rundown-view/components/minishelf/minishelf.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core'
 import { Logger } from '../../../core/abstractions/logger.service'
 import { Segment } from '../../../core/models/segment'
-import { Part } from '../../../core/models/part'
 
 @Component({
   selector: 'sofie-minishelf',
@@ -9,10 +8,12 @@ import { Part } from '../../../core/models/part'
   styleUrls: ['./minishelf.component.scss'],
 })
 export class MinishelfComponent {
+  @Input()
+  public segment: Segment
+
   private readonly logger: Logger
   private readonly data: { prop: string }
-  @Input() segment!: Segment
-  private readonly part: Part
+
   constructor(logger: Logger) {
     this.logger = logger.tag('MinishelfComponent')
     this.data = { prop: 'value' }
