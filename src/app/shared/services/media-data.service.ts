38f6ee17d52756398b8cd3118b79081928603a2f
Dimitar G. Mustakinov <k-primeholding-dim@tv2.dk>
2024-01-18 22:21:48 +0200

    SOF-1720 - post recovery lint

--- src/app/shared/services/media-data.service.ts
+++ src/app/shared/services/media-data.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
 import { MediaData } from './media-data'
 
-
 @Injectable()
 export abstract class MediaDataService {
   public abstract getMediaDurationById(id: string): Observable<MediaData>
