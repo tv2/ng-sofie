82b045a4caf0bc89cd6e12d1e454889706e9e7e5
Dimitar G. Mustakinov <k-primeholding-dim@tv2.dk>
2024-01-19 16:00:39 +0200

    SOF-1720 - fises two tests

--- src/app/rundown-view/components/minishelf/mini-shelf.component.spec.ts
+++ src/app/rundown-view/components/minishelf/mini-shelf.component.spec.ts
@@ -4,6 +4,7 @@ import { ConfigurationService } from '../../../shared/services/configuration-ser
 import { instance, mock } from '@typestrong/ts-mockito'
 import { ActionStateService } from '../../../shared/services/action-state.service'
 import { MediaDataService } from '../../../shared/services/media-data.service'
+import { TimerPipe } from '../../../shared/pipes/timer/timer.pipe'
 
 describe('MiniShelfComponent', () => {
   it('should create', async () => {
@@ -24,7 +25,7 @@ async function configureTestBed(
   const mockedMediaDataService: MediaDataService = params.mockedMediaDataService ?? mock<MediaDataService>()
 
   await TestBed.configureTestingModule({
-    declarations: [MiniShelfComponent],
+    declarations: [MiniShelfComponent, TimerPipe],
     providers: [
       { provide: ConfigurationService, useValue: instance(mockedConfigurationService) },
       { provide: ActionStateService, useValue: instance(mockedActionStateService) },
