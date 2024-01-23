81787e8c94d3394d1c9a39da8205c79b288f6ebc
Dimitar G. Mustakinov <k-primeholding-dim@tv2.dk>
2024-01-23 12:16:05 +0200

    SOF-1720 - adds HttpConfigurationService simple test

--- src/app/shared/services/http/http-configuration.service.spec.ts
+++ src/app/shared/services/http/http-configuration.service.spec.ts
@@ -1,18 +1,18 @@
 import { instance, mock } from '@typestrong/ts-mockito'
 import { HttpClient } from '@angular/common/http'
 import { HttpErrorService } from './http-error.service'
-import { HttpMediaService } from './http-media.service'
 import { EntityParser } from '../../../core/abstractions/entity-parser.service'
+import { HttpConfigurationService } from './http-configuration-service'
 
-describe(HttpMediaService.name, () => {
-  describe(HttpMediaService.prototype.getMedia.name, () => {
+describe(HttpConfigurationService.name, () => {
+  describe(HttpConfigurationService.prototype.getStudioConfiguration.name, () => {
     it('should create', () => {
       const mockedHttpClient: HttpClient = mock<HttpClient>()
       const mockedHttpErrorService: HttpErrorService = mock<HttpErrorService>()
       const mockedEntityParser: EntityParser = mock<EntityParser>()
-      const testee: HttpMediaService = new HttpMediaService(instance(mockedHttpClient), instance(mockedHttpErrorService), instance(mockedEntityParser))
+      const testee: HttpConfigurationService = new HttpConfigurationService(instance(mockedHttpClient), instance(mockedHttpErrorService), instance(mockedEntityParser))
       expect(testee).toBeTruthy()
-      expect(testee.getMedia).toBeTruthy()
+      expect(testee.getStudioConfiguration).toBeTruthy()
     })
   })
 })
