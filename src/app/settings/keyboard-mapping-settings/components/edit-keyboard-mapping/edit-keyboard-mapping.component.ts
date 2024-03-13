ed36da215942761938b756bfa0ff701b5a418c8b
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2024-03-13 08:00:43 +0100

    SOF-1748 Create EditKeyboardMappingComponent so it is no longer always bound to being a Dialog

---
+++ src/app/settings/keyboard-mapping-settings/components/edit-keyboard-mapping/edit-keyboard-mapping.component.ts
@@ -0,0 +1,103 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
+import { ActionArgumentSchemaType } from '../../../../shared/models/action'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { KeyEventType } from '../../../../keyboard/value-objects/key-event-type'
+import { KeyboardMapping } from '../keyboard-mapping-settings-page/keyboard-mapping-settings-page.component'
+import { Tv2Action, Tv2PartAction } from '../../../../shared/models/tv2-action'
+import { MultiSelectOption } from '../../../../shared/components/multi-select/multi-select.component'
+
+@Component({
+  selector: 'sofie-edit-keyboard-mapping',
+  templateUrl: './edit-keyboard-mapping.component.html',
+  styleUrls: ['./edit-keyboard-mapping.component.scss'],
+})
+export class EditKeyboardMappingComponent implements OnInit {
+  protected readonly ActionArgumentSchemaType = ActionArgumentSchemaType
+
+  protected readonly labelLabel: string = $localize`global.label`
+  protected readonly shortcutLabel: string = $localize`action-triggers.shortcut.label`
+  protected readonly physicalMappingLabel: string = $localize`action-triggers.physical-mapping.label`
+  protected readonly physicalMappingHelpText: string = $localize`action-triggers.physical-mapping-help.tooltip`
+  protected readonly triggerOnLabel: string = $localize`action-triggers.trigger-on.label`
+
+  @Input() public keyboardMapping?: KeyboardMapping
+
+  @Output() public onSave: EventEmitter<KeyboardMapping> = new EventEmitter()
+  @Output() public onCancel: EventEmitter<void> = new EventEmitter()
+
+  public actionTriggerForm: FormGroup
+  public actionTriggerDataForm: FormGroup
+
+  public triggerOnOptions: MultiSelectOption<KeyEventType>[] = []
+
+  public selectedAction?: Tv2Action
+
+  constructor(private readonly formBuilder: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this.actionTriggerDataForm = this.formBuilder.group({
+      label: [this.keyboardMapping?.actionTrigger.data.label ?? ''],
+      keys: [this.keyboardMapping?.actionTrigger.data.keys, [Validators.required]],
+      triggerOn: [KeyEventType.RELEASED, [Validators.required]],
+      mappedToKeys: [[]],
+    })
+
+    this.actionTriggerForm = this.formBuilder.group({
+      actionId: ['', [Validators.required]],
+      data: this.actionTriggerDataForm,
+    })
+
+    this.triggerOnOptions = Object.values(KeyEventType).map(keyEventType => {
+      return {
+        name: keyEventType.toString(),
+        value: keyEventType,
+      }
+    })
+
+    if (this.keyboardMapping) {
+      this.selectAction(this.keyboardMapping?.action!)
+    }
+  }
+
+  public selectAction(action: Tv2Action): void {
+    this.selectedAction = action
+
+    this.actionTriggerForm.patchValue({
+      actionId: action.id,
+    })
+
+    this.doesSelectedActionHaveActionArguments() ? this.addActionArgumentsFormControl() : this.removeActionArgumentsFormControl()
+  }
+
+  private addActionArgumentsFormControl(): void {
+    this.actionTriggerDataForm.setControl('actionArguments', this.formBuilder.nonNullable.control(this.keyboardMapping?.actionTrigger.data.actionArguments, Validators.required))
+  }
+
+  private removeActionArgumentsFormControl(): void {
+    this.actionTriggerDataForm.removeControl('actionArguments')
+  }
+
+  public doesSelectedActionHaveActionArguments(): boolean {
+    return !!this.selectedAction && !!this.selectedAction.argument
+  }
+
+  public getSelectedActionArgumentType(): ActionArgumentSchemaType {
+    return this.selectedAction!.argument!.type
+  }
+
+  public save(): void {
+    this.keyboardMapping = {
+      action: {} as Tv2PartAction,
+      actionTrigger: {
+        ...this.keyboardMapping?.actionTrigger,
+        ...this.actionTriggerForm.value,
+      },
+    }
+
+    this.onSave.emit(this.keyboardMapping)
+  }
+
+  public cancel(): void {
+    this.onCancel.emit()
+  }
+}
--- src/app/settings/keyboard-mapping-settings/keyboard-mapping-settings.module.ts
+++ src/app/settings/keyboard-mapping-settings/keyboard-mapping-settings.module.ts
@@ -5,12 +5,13 @@ import { SettingsSharedModule } from '../settings-shared/settings-shared.module'
 import { KeyboardMappingSettingsPageComponent } from './components/keyboard-mapping-settings-page/keyboard-mapping-settings-page.component'
 import { ProducerShelfModule } from '../../producer-shelf/producer-shelf.module'
 import { EditKeyboardMappingDialogComponent } from './components/edit-keyboard-mapping-dialog/edit-keyboard-mapping-dialog.component'
-import { ActionSelectorComponent } from './components/action-selector/action-selector.component'
+import { ActionSelectorComponent } from './components/action-selector/action-selector.component';
+import { EditKeyboardMappingComponent } from './components/edit-keyboard-mapping/edit-keyboard-mapping.component'
 
 const routes: Routes = [{ path: '', component: KeyboardMappingSettingsPageComponent }]
 
 @NgModule({
-  declarations: [KeyboardMappingSettingsPageComponent, EditKeyboardMappingDialogComponent, ActionSelectorComponent],
+  declarations: [KeyboardMappingSettingsPageComponent, EditKeyboardMappingDialogComponent, ActionSelectorComponent, EditKeyboardMappingComponent],
   imports: [SharedModule, RouterModule.forChild(routes), SettingsSharedModule, ProducerShelfModule],
   providers: [],
 })
