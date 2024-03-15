b84ef27c9a7c57e53f0aeed2c6653bfed9a4e6cd
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2024-03-15 08:24:19 +0100

    SOF-1748 Refactor SettingsMenu to use less CSS and not having to hardcode every single MenuItem

--- src/app/settings/components/settings-menu/settings-menu.component.ts
+++ src/app/settings/components/settings-menu/settings-menu.component.ts
@@ -4,25 +4,52 @@ import { Subject, takeUntil } from 'rxjs'
 import { SettingsPath } from 'src/app/settings/settings-routing.module'
 import { ShelfSettingsPath } from '../../shelf-settings/shelf-settings.module'
 
+interface MenuItem {
+  name: string
+  routerLink: string[]
+  shouldShowSubItems?: () => boolean
+  subItems?: MenuItem[]
+}
+
 @Component({
   selector: 'sofie-settings-menu',
   templateUrl: './settings-menu.component.html',
   styleUrls: ['./settings-menu.component.scss'],
 })
 export class SettingsMenuComponent implements OnInit, OnDestroy {
-  public SettingsPath = SettingsPath
-  public ShelfPath = ShelfSettingsPath
-  public isShelfRouteActive: boolean = false
+  public menuItems: MenuItem[] = [
+    {
+      name: $localize`settings.keyboard-mappings.label`,
+      routerLink: [SettingsPath.KEYBOARD_MAPPINGS],
+    },
+    {
+      name: $localize`settings.shelf.label`,
+      routerLink: [SettingsPath.SHELF],
+      shouldShowSubItems: (): boolean => this.isShelfRouteActive,
+      subItems: [
+        {
+          name: $localize`settings.shelf.action-panels.label`,
+          routerLink: [SettingsPath.SHELF, ShelfSettingsPath.ACTION_PANELS],
+        },
+      ],
+    },
+    {
+      name: $localize`settings.clear-cache.label`,
+      routerLink: [SettingsPath.CLEAR_CACHE],
+    },
+  ]
+
+  private isShelfRouteActive: boolean = false
 
   private readonly unsubscribe$: Subject<void> = new Subject<void>()
 
   constructor(private readonly router: Router) {}
 
   public ngOnInit(): void {
-    this.isShelfRouteActive = this.router.url.includes(this.SettingsPath.SHELF)
+    this.isShelfRouteActive = this.router.url.includes(SettingsPath.SHELF)
     this.router.events.pipe(takeUntil(this.unsubscribe$)).subscribe(routerEvent => {
       if (routerEvent instanceof NavigationEnd) {
-        this.isShelfRouteActive = routerEvent.url.includes(this.SettingsPath.SHELF)
+        this.isShelfRouteActive = routerEvent.url.includes(SettingsPath.SHELF)
       }
     })
   }
