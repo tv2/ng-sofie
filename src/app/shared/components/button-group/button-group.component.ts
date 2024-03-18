391163e266aabf728667f5f437abf9e885cec840
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2024-03-18 13:32:58 +0100

    SOF-1748 Remove viewEncapsulation and simplify CSS

--- src/app/shared/components/button-group/button-group.component.ts
+++ src/app/shared/components/button-group/button-group.component.ts
@@ -1,10 +1,9 @@
-import { Component, ViewEncapsulation } from '@angular/core'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'sofie-button-group',
   templateUrl: './button-group.component.html',
   styleUrls: ['./button-group.component.scss'],
-  encapsulation: ViewEncapsulation.None,
 })
 export class ButtonGroupComponent {
   constructor() {}
