b63b3fe09387fb76471db93de2175c81949aed35
L0265\viktor.ivanov_primeh <k-primeholding-vi@tv2.dk>
2024-03-20 15:17:23 +0200

    SOF-1676 - add dark and light theme to confirm dialogs

--- src/app/shared/components/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/shared/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -8,6 +8,11 @@ export enum DialogSeverity {
   DANGER = 'DANGER',
 }
 
+export enum DialogColorTheme {
+  LIGHT = 'LIGHT',
+  DARK = 'DARK',
+}
+
 export interface ConfirmationDialogData {
   title?: string
   message?: string
@@ -16,6 +21,7 @@ export interface ConfirmationDialogData {
     cancel?: string
   }
   severity?: DialogSeverity
+  theme?: DialogColorTheme
 }
 
 @Component({
@@ -28,7 +34,10 @@ export class ConfirmationDialogComponent extends StronglyTypedDialog<Confirmatio
   public message: string = ''
   public okButtonText: string = $localize`confirmation-dialog.yes.button`
   public cancelButtonText: string = $localize`confirmation-dialog.cancel.button`
+  public theme: DialogColorTheme
   public severity: DialogSeverity
+
+  protected readonly DialogColorTheme = DialogColorTheme
 
   @HostBinding('class')
   public get severityClass(): string {
@@ -54,5 +63,6 @@ export class ConfirmationDialogComponent extends StronglyTypedDialog<Confirmatio
     this.okButtonText = data.buttonText?.ok ?? this.okButtonText
     this.cancelButtonText = data.buttonText?.cancel ?? this.cancelButtonText
     this.severity = data.severity ?? DialogSeverity.INFO
+    this.theme = data.theme ?? DialogColorTheme.LIGHT
   }
 }
