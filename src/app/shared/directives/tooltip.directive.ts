16ef60617f1c9263f399e14988495ad3eba9b42b
Anders Frederik Jørgensen <afjo@tv2.dk>
2024-03-25 15:11:51 +0100

    fix: Tooltip position is based on the bounding rectangle of the element with the directive attribute.

--- src/app/shared/directives/tooltip.directive.ts
+++ src/app/shared/directives/tooltip.directive.ts
@@ -1,4 +1,16 @@
-import { ApplicationRef, ComponentRef, createComponent, Directive, EventEmitter, HostListener, Input, OnDestroy, Output, TemplateRef } from '@angular/core'
+import {
+  ApplicationRef,
+  ComponentRef,
+  createComponent,
+  Directive,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  OnDestroy,
+  Output,
+  TemplateRef
+} from '@angular/core'
 import { TooltipComponent } from '../components/tooltip/tooltip.component'
 
 const MINIMUM_HORIZONTAL_OFFSET_IN_PIXELS: number = 0
@@ -17,7 +29,10 @@ export class TooltipDirective implements OnDestroy {
 
   private tooltipComponentRef: ComponentRef<TooltipComponent>
 
-  constructor(private readonly applicationRef: ApplicationRef) {}
+  constructor(
+    private readonly applicationRef: ApplicationRef,
+    private readonly triggerElement: ElementRef
+  ) {}
 
   @HostListener('mouseenter', ['$event'])
   public showTooltip(event: MouseEvent): void {
@@ -42,8 +57,7 @@ export class TooltipDirective implements OnDestroy {
       return
     }
 
-    const triggerElement: HTMLElement | null = event.target as HTMLElement | null
-    const verticalPositionInPixels: number = triggerElement?.getBoundingClientRect().top ?? 0
+    const verticalPositionInPixels: number = this.triggerElement.nativeElement.getBoundingClientRect().top ?? 0
     const tooltipWidthInPixels: number = this.tooltipComponentRef.instance.getWidthInPixels()
 
     const horizontalOffsetInPixels: number = Math.max(event.clientX - Math.ceil(tooltipWidthInPixels / 2), MINIMUM_HORIZONTAL_OFFSET_IN_PIXELS)
