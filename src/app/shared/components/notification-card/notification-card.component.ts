fdc41a6bb0105bf743b3b512406ea45651ab8478
L0265\viktor.ivanov_primeh <k-primeholding-vi@tv2.dk>
2024-04-09 14:38:48 +0300

    SOF-1861 - move notification styles to css

--- src/app/shared/components/notification-card/notification-card.component.ts
+++ src/app/shared/components/notification-card/notification-card.component.ts
@@ -1,15 +1,6 @@
 import { Component, ElementRef, Input, OnInit } from '@angular/core'
 import { Icon, IconSize } from '../../enums/icon'
 import { Notification } from '../../models/notification'
-import { StatusCode } from '../../enums/status-code'
-
-const INFO_COLOR: string = 'var(--info-color)'
-const WARNING_COLOR: string = 'var(--attention-color)'
-const ERROR_COLOR: string = 'var(--danger-color)'
-
-const GRADIENT_DEGREE: string = 'var(--gradient-degree)'
-const STATUS_COLOR_WIDTH: string = 'var(--status-color-width)'
-const BACKGROUND_COLOR: string = 'var(--white-color)'
 
 @Component({
   selector: 'sofie-notification-card',
@@ -32,27 +23,14 @@ export class NotificationCardComponent implements OnInit {
   constructor(private readonly elementRef: ElementRef) {}
 
   public ngOnInit(): void {
-    this.setBackground()
+    this.setClassName()
   }
 
   public remove(): void {
     this.onRemoveCallback(this.notification)
   }
 
-  private setBackground(): void {
-    const statusColor: string = this.getStatusColor()
-    this.elementRef.nativeElement.style.background = `linear-gradient(${GRADIENT_DEGREE}, ${statusColor}, ${statusColor} ${STATUS_COLOR_WIDTH}, ${BACKGROUND_COLOR} ${STATUS_COLOR_WIDTH}, ${BACKGROUND_COLOR})`
-  }
-
-  private getStatusColor(): string {
-    switch (this.notification.statusCode) {
-      case StatusCode.BAD:
-        return ERROR_COLOR
-      case StatusCode.WARNING:
-        return WARNING_COLOR
-      case StatusCode.GOOD:
-      case StatusCode.UNKNOWN:
-        return INFO_COLOR
-    }
+  private setClassName(): void {
+    this.elementRef.nativeElement.classList.add(this.notification.statusCode.toLocaleLowerCase())
   }
 }
