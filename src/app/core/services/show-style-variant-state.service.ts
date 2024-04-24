d70317ae5c0375497e10d1e7ab077438aa4bfbb5
Rasmus <raal@tv2.dk>
2024-04-24 14:27:49 +0200

    fix: The show style variant now listens to rundown updated events

--- src/app/core/services/show-style-variant-state.service.ts
+++ src/app/core/services/show-style-variant-state.service.ts
@@ -3,13 +3,40 @@ import { BehaviorSubject, lastValueFrom, Observable } from 'rxjs'
 import { ShowStyleVariant } from '../models/show-style-variant'
 import { ShowStyleVariantService } from '../abstractions/show-style-variant.service'
 import { EventSubscription } from '../../event-system/abstractions/event-observer.service'
+import { RundownEventObserver } from './rundown-event-observer.service'
+import { RundownUpdatedEvent } from '../models/rundown-event'
+import {Logger} from "../abstractions/logger.service";
 
 @Injectable()
 export class ShowStyleVariantStateService implements OnDestroy {
   private readonly showStyleVariantSubjects: Map<string, BehaviorSubject<ShowStyleVariant>> = new Map()
   private readonly eventSubscriptions: EventSubscription[]
+  private readonly logger: Logger
 
-  constructor(private readonly showStyleVariantService: ShowStyleVariantService) {}
+  constructor(
+    private readonly showStyleVariantService: ShowStyleVariantService,
+    private readonly rundownEventObserver: RundownEventObserver,
+    logger: Logger
+  ) {
+    this.logger = logger.tag('ShowStyleVariantStateService')
+    this.subscribeToRundownEvents()
+  }
+
+  private subscribeToRundownEvents(): void {
+    this.rundownEventObserver.subscribeToRundownUpdates(this.updateShowStyleVariantFromEvent.bind(this))
+  }
+
+  private updateShowStyleVariantFromEvent(event: RundownUpdatedEvent): void {
+    this.getCleanShowStyleVariantSubject(event.rundownId)
+      .then(newVariant => {
+        const showStyleVariantSubject: BehaviorSubject<ShowStyleVariant> | undefined = this.showStyleVariantSubjects.get(event.rundownId)
+        if (!showStyleVariantSubject) {
+          return
+        }
+        showStyleVariantSubject.next(newVariant.value)
+      })
+      .catch(error => this.logger.data(error).error('Failed getting clean showstyle variant subject.'))
+  }
 
   public async subscribeToShowStyleVariant(rundownId: string): Promise<Observable<ShowStyleVariant>> {
     const showStyleVariantSubject: BehaviorSubject<ShowStyleVariant> = await this.getShowStyleVariantSubject(rundownId)
@@ -21,12 +48,12 @@ export class ShowStyleVariantStateService implements OnDestroy {
     if (showStyleVariantSubject) {
       return showStyleVariantSubject
     }
-    const cleanShowStyleVariantSubject = await this.getCleanRundownSubject(rundownId)
+    const cleanShowStyleVariantSubject = await this.getCleanShowStyleVariantSubject(rundownId)
     this.showStyleVariantSubjects.set(rundownId, cleanShowStyleVariantSubject)
     return cleanShowStyleVariantSubject
   }
 
-  private async getCleanRundownSubject(rundownId: string): Promise<BehaviorSubject<ShowStyleVariant>> {
+  private async getCleanShowStyleVariantSubject(rundownId: string): Promise<BehaviorSubject<ShowStyleVariant>> {
     const showStyleVariant: ShowStyleVariant = await this.fetchShowStyleVariant(rundownId)
     return new BehaviorSubject<ShowStyleVariant>(showStyleVariant)
   }
